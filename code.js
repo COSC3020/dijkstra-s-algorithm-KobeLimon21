938466b4ba8ec2d782743efd549f66bc73707661
KobeLimon21 <kobelimon2121@gmail.com>
2024-10-15 21:05:20 -0600

    Update code.js

--- code.js
+++ code.js
@@ -13,11 +13,11 @@ function dijkstra(graph, sourceNode) {  // adjacency list
     }
     let marked = new Array(matrix).fill(0); // unmarked nodes set as 0 
 
-     for (let i = 0; i < matrix; i++) { // runs through nodes
+    for (let i = 0; i < matrix; i++) { // runs through nodes
         let minDist = Infinity; // set node to infinity
         let minVertex = -1; // no node chosen yet so gets set to -1 
 
-        for (let vertex = 0; vertex < matrix; vertex++) { // runs through all nodes to find unmarked mode with smallest distance
+        for (let vertex = 0; vertex < matrix; vertex++) { // runs through all nodes to find unmarked node with smallest distance
             if (marked[vertex] === 0 && dist[vertex] < minDist) { // checks if current unmarked node has a shorter distance 
                 minDist = dist[vertex]; // updates new found distance
                 minVertex = vertex; // updates node with new distance 
@@ -29,8 +29,9 @@ function dijkstra(graph, sourceNode) {  // adjacency list
         }
         marked[minVertex] = 1; // sets marked vertex as 1 to note it was visited 
 
-        for (let neighbor in graph[minVertex]) { // runs through neighbors 
-            let edgeWeight = graph[minVertex][neighbor]; // gets cost of edge between neighbor and min vertex 
+        for (let j = 0; j < graph[minVertex].length; j++) { // runs through neighbors of node 
+            let neighbor = graph[minVertex][j][0];      // index of neighbor node
+            let edgeWeight = graph[minVertex][j][1];    // edge weight to the neighbor
             let newDist = dist[minVertex] + edgeWeight; // computes possible new shortest distance 
 
             if (newDist < dist[neighbor]) { // checks if it is shorter
@@ -43,5 +44,3 @@ function dijkstra(graph, sourceNode) {  // adjacency list
 }
 
 
-
-     
